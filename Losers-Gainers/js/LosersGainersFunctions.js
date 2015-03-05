6a128bd0732870b40d2b2108adf046a034c21466
Jeremy Zastawny <jayzedeh@gmail.com>
2015-03-05 18:01:32 -0500

    so confused on how to get data from database and save into array, someone else can try

--- Losers-Gainers/js/LosersGainersFunctions.js
+++ Losers-Gainers/js/LosersGainersFunctions.js
@@ -1,19 +1,28 @@
-/*function gainerSearch(){
-    For(i)data.Math.max(a,b)
-}
-
-function loserSearch(){
-
-}
-*/
+'use strict';
 
 //adds a row
 function AddRowFunction() {
     var table = document.getElementById("table1");
     var row = table.insertRow(1);
     var cell1 = row.insertCell(0), cell2 = row.insertCell(1), cell3 = row.insertCell(2), cell4 = row.insertCell(3);
-    var loserName = "AAPL",loserChange = "-2%",gainerName = "TSLA",gainerChange = "6%"
+    var loserName = "AAPL",loserChange = "-2%",gainerName = "TSLA",gainerChange = "6%";
 
+/*  Below is the beginning of some array system
+
+    var array = [];
+    var dayOne = 250 , dayTwo = 50;
+    var x = 0, y = 1;
+   
+    Math.max(array); 
+    
+    x++;
+    y++;
+
+    //input respective stock name
+    array[x] = data.;
+    array[y] = ((dayTwo - dayOne) / dayOne) * 100;
+    console.log(array[x] + " " + array[y] + "%");
+*/
     cell1.innerHTML = loserName;
     cell2.innerHTML = loserChange;
     cell3.innerHTML = gainerName;
@@ -28,4 +37,3 @@ function deleteRow(rowNum) {
     document.getElementById("table1").deleteRow(i);
     }
 }
-
